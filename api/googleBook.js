6789d19726811540e34d0b27f710fe38e21195cf
Kevin Jayat <kevin.j@tymate.com>
2022-11-17 18:06:50 +0100

    add: env google book

--- api/googleBook.js
+++ api/googleBook.js
@@ -1,7 +1,4 @@
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { camelizeKeys, decamelizeKeys } from "humps";
-
-const googleBookApi = axios.create({
-  baseURL: "https://www.googleapis.com/auth/books",
-});
+export const API_KEY = "AIzaSyD9zphCi40jeHwuYnqlwJ7epLivAHzv6gk";
+export const GOOGLE_BOOK_URL = "https://www.googleapis.com/auth/books";
+export const GET_BOOKS_BY_NAME_ENDPOINT = "/v1/volumes?q=";
+export const KEY_HEADER = "&key=" + API_KEY;
